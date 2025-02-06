1a31373b53fcc9d47aca84c21c6dbaaa02ec9d44
ShirleyFang <xiaoxue.fang.dream@gmail.com>
2025-02-05 23:32:35 -0800

    reorganized the tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,15 +5,12 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    # def test_foo(self):
-    #     items = [Item("foo", 0, 0)]
 
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
         items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        # self.assertEquals("fixme", items[0].name)
         sulfuras_item = items[0]
         self.assertEqual(80, sulfuras_item.quality)
         self.assertEqual(4, sulfuras_item.sell_in)
@@ -33,18 +30,15 @@ class GildedRoseTest(unittest.TestCase):
         all_items = gilded_rose.items_name
         self.assertEquals(["Sulfuras"], all_items)
 
-    # example of test that checks for logical errors
+    # tests that checks for logical errors
     # 1. Quality should never greater than 50
     def test_sulfuras_should_not_be_created(self):
         items = [Item("Sulfuras", 5, 55)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        # self.assertEquals("fixme", items[0].name)
         sulfuras_item = items[0]
-        self.assertEqual(50, sulfuras_item.quality)
-        # self.assertEqual(4, sulfuras_item.sell_in)
-        # self.assertEqual("Sulfuras", sulfuras_item.name)
-    
+        self.assertLessEqual(sulfuras_item.quality, 50) # the value should always less than 50
+
     # 2. Quality should never be negative
     def test_sulfuras_should_not_instantiated_negativce_quality(self):
         items = [Item("Sulfuras", 3, -1)]
@@ -52,15 +46,16 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         # self.assertEquals("fixme", items[0].name)
         sulfuras_item = items[0]
-        self.assertEqual(0, sulfuras_item.quality)
-    
-    # 3. Based on the logic, it should not decrease 2
+        self.assertGreaterEqual(sulfuras_item.quality, 0) # the quality shoulg always greater than 0
+
+    # 3. Based on the description("Conjured" items degrade in Quality twice as fast as normal items), it should not decrease 2
     def test_sulfuras_should_decrease_one(self):
         items = [Item("Backstage passes", 0, 24)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
         self.assertEqual(23, sulfuras_item.quality)
+
 
 if __name__ == '__main__':
     unittest.main()
