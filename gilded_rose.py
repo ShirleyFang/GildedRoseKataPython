d6b92cd8d0dd403afb2c966d6186205bf1b4c906
ShirleyFang <xiaoxue.fang.dream@gmail.com>
2025-02-12 22:35:45 -0800

    refactor the grided_rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+TWO = 2
+FOUR = 4
+ONE = 1
+
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
@@ -12,38 +18,102 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class SubItem(Item):
+    def __init__(self, name, sell_in, quality):
+        quality = max(MIN_QUALITY, min(quality, MAX_QUALITY))
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        pass
+
+
+class NormalItem(SubItem):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def validate_quality(self):
+        return self.quality > MIN_QUALITY and self.quality <= MAX_QUALITY
+
+    def update_quality(self):
+        if self.sell_in >= MIN_QUALITY and self.quality > ONE:
+            self.quality -= ONE
+        elif self.sell_in < MIN_QUALITY and self.quality > TWO: 
+            self.quality -= TWO
+        else:
+            self.quality = MIN_QUALITY
+        self.sell_in -= ONE
+
+
+class AgedBried(SubItem):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        self.sell_in -= ONE
+        if (self.quality < MAX_QUALITY):
+            self.quality += ONE
+
+
+class SulfurasItem(SubItem):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        self.sell_in -= ONE
+
+
+class BackstagePassesItem(SubItem):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        FIVE_DAYS = 5
+        TEN_DAYS = 10
+        THREE = 3
+
+        if self.sell_in == MIN_QUALITY:
+            self.quality = MIN_QUALITY
+        elif self.sell_in <= FIVE_DAYS:
+            self.quality = min(MAX_QUALITY, self.quality + THREE)
+        elif self.sell_in <= TEN_DAYS:
+            self.quality = min(MAX_QUALITY, self.quality + TWO)
+
+        self.sell_in -= ONE
+
+
+class ConjuredItem(SubItem):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+
+        if self.sell_in >= MIN_QUALITY and self.quality > TWO:
+            self.quality -= TWO
+        elif self.sell_in < MIN_QUALITY and self.quality > FOUR:
+            self.quality -= FOUR
+        else:
+            self.quality = MIN_QUALITY
+        self.sell_in -= ONE
+
+
 class GildedRose(object):
 
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
+    def get_items_name(self):
+        item_names = [item.name for item in self.items]
+        return item_names
+
+    def get_items(self):
+        return self.items
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality()
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,56 +1,72 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+
+from gilded_rose import SulfurasItem, AgedBried, ConjuredItem, BackstagePassesItem, NormalItem, Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
 
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [SulfurasItem("Sulfuras", 5, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
-        self.assertEqual(80, sulfuras_item.quality)
+        self.assertEqual(50, sulfuras_item.quality)
         self.assertEqual(4, sulfuras_item.sell_in)
         self.assertEqual("Sulfuras", sulfuras_item.name)
 
     # example of test that checks for syntax errors
     def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [SulfurasItem("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_items()
-        self.assertEqual(["Sulfuras"], all_items)
+        self.assertEqual(items, all_items)
 
-    # test syntex errors-calling a unexited attribute in the class of GildedRose
-    def test_gilded_rose_list_all_items_syntax_error(self):
-        items = [Item("Sulfuras", 2, 50)]
+    # test syntax errors-calling a unexited attribute in the class of GildedRose
+    def test_gilded_rose_list_all_items_syntax_error1(self):
+        items = [SulfurasItem("Sulfuras", 2, 50)]
         gilded_rose = GildedRose(items)
-        all_items = gilded_rose.items_name
-        self.assertEquals(["Sulfuras"], all_items)
+        all_items = gilded_rose.get_items_name()
+        self.assertEqual(["Sulfuras"], all_items)
+    
+     # test syntex errors-calling a unexited attribute in the class of GildedRose
+    def test_gilded_rose_list_all_items_syntax_error(self):
+        items = [SulfurasItem("Sulfuras", 2, 50)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.items
+        self.assertTrue(
+            all(item1.name == item2.name and
+                item1.sell_in == item2.sell_in and
+                item1.quality == item2.quality
+                for item1, item2 in zip([SulfurasItem("Sulfuras", 2, 50)], all_items))
+        )
+
 
     # tests that checks for logical errors
     # 1. Quality should never greater than 50
     def test_sulfuras_should_not_be_created(self):
-        items = [Item("Sulfuras", 5, 55)]
+        items = [SulfurasItem("Sulfuras", 5, 55)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
+
         self.assertLessEqual(sulfuras_item.quality, 50) # the value should always less than 50
 
     # 2. Quality should never be negative
     def test_sulfuras_should_not_instantiated_negativce_quality(self):
-        items = [Item("Sulfuras", 3, -1)]
+        items = [SulfurasItem("Sulfuras", 3, -1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         # self.assertEquals("fixme", items[0].name)
         sulfuras_item = items[0]
+        print(sulfuras_item.quality)
         self.assertGreaterEqual(sulfuras_item.quality, 0) # the quality shoulg always greater than 0
 
     # 3. Based on the description("Conjured" items degrade in Quality twice as fast as normal items), it should not decrease 2
     def test_sulfuras_should_decrease_one(self):
-        items = [Item("Backstage passes", 0, 24)]
+        items = [NormalItem("Backstage passes", 0, 24)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
